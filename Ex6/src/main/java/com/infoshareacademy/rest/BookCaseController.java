b88c0c5204e6faf6f25c0e4e9d79c6ddf08b2a3f
PrzemyslawWanagos <wanagos@interia.eu>
2021-12-10 13:28:47 +0100

    1. exit button added - not nice work :( 2. removed starting of browser as it is MS Windows depending

--- Ex6/src/main/java/com/infoshareacademy/rest/BookCaseController.java
+++ Ex6/src/main/java/com/infoshareacademy/rest/BookCaseController.java
@@ -5,6 +5,10 @@ import com.infoshareacademy.service.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static com.infoshareacademy.BookcaseApp.exitApp;
+
 
 @Controller
 public class BookCaseController {
@@ -20,6 +24,15 @@ public class BookCaseController {
     public String mainPage() {
         books.setBooks(Services.readBookCase().getBooks());
         return "main";
+    }
+
+    @GetMapping("/exit")
+    @ResponseBody
+    public void exit() {
+        try {
+            exitApp();
+        } catch (Exception e) {
+        }
     }
 
 }
