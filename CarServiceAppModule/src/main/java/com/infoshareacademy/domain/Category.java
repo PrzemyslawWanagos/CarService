7767347db6655b33804f7756ec75502243878b0f
PRZWAN <wanagos@interia.eu>
2021-12-16 16:42:50 +0100

    refactor continuing

--- CarServiceAppModule/src/main/java/com/infoshareacademy/domain/Category.java
+++ CarServiceAppModule/src/main/java/com/infoshareacademy/domain/Category.java
@@ -2,12 +2,12 @@ package com.infoshareacademy.domain;
 
 public enum Category {
 
-    SEATS ("Crime, Thriller"),
-    BRAKES("Fiction"),
-    MAINTENANCE("Historical"),
-    OTHER("Science Fiction"),
-    DRIVE("Biography"),
-    ENGINE("Romance");
+    SEATS ("Problems with seats"),
+    BRAKES("Problems with brakes"),
+    MAINTENANCE("General maintenance"),
+    OTHER("Other problems"),
+    DRIVE("Problems with drive"),
+    ENGINE("Problems with engine");
 
     private String categoryName;
 
