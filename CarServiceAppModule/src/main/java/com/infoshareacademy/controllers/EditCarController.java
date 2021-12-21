b37fc12926d2982efa5a826658e9f61c2c5affd3
PRZWAN <wanagos@interia.eu>
2021-12-21 11:04:55 +0100

    completed: 1. end points for edit/add to be unified (varioes requests (GET/POST) can use the same address) 2. investigate mapping between carDTO and car - maybe can be done with mapping not one by one property 6. use for each in thymleaf to prepopulate drop down list
    In progress:
    5. validation:
    - front end - EZPZ
    - back end - AddCarController BindingReults throws error when make is shorter than 5 chars or longer than 10. to be expanded on other fields and to highlight it in html form.
    
    Side improvement:
    1. refactoring of html from SearchCar to search-car and the same with other similarly
    
    Next steps:
    3. move methods between DTO/Services/Domain
    4. verify annotations
    7. set up language and file format (json vs txt vs other)
    8. CSS formatting

--- CarServiceAppModule/src/main/java/com/infoshareacademy/controllers/EditCarController.java
+++ CarServiceAppModule/src/main/java/com/infoshareacademy/controllers/EditCarController.java
@@ -5,11 +5,8 @@ import com.infoshareacademy.dto.CarDto;
 import com.infoshareacademy.repository.Cars;
 import com.infoshareacademy.service.Services;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,7 +15,7 @@ public class EditCarController {
 
     private final Services services;
     private Cars cars;
-    private Integer CarToEditID;
+    private Integer carToEditID;
 
 
     @Autowired
@@ -29,25 +26,22 @@ public class EditCarController {
 
     @RequestMapping(value = "/edit/{licencePlate}", method = RequestMethod.GET)
     public ModelAndView carEditForm(@PathVariable String licencePlate) {
-        CarToEditID= services.FindByLicencePlate(cars, licencePlate);
+        carToEditID = services.FindByLicencePlate(cars, licencePlate);
+        Car carToEdit = cars.getCars().get(carToEditID);
+        CarDto carDto=new CarDto();
+        services.fromEntityToDto(carToEdit,carDto);
         ModelAndView modelAndView=new ModelAndView("edit-cars");
-        Car CarToEdit = cars.getCars().get(CarToEditID);
-        modelAndView.addObject("carDto", CarToEdit);
+        modelAndView.addObject("carDto", carDto);
         return modelAndView;
     }
 
     @PostMapping(value = "/edit/{licencePlate}")
     public String saveEditedCar(CarDto carDto) {
-                Car car = cars.getCars().get(CarToEditID);
+                Car car = cars.getCars().get(carToEditID);
         try {
-            car.setMake(carDto.getMake());
-
-            car.setLicencePlate(carDto.getLicencePlate());
+            services.fromDtoToEntity(carDto,car);
             car.setCostOfService(carDto.getCostOfService());
-            car.setCategory(carDto.getCategory());
-
            car.setRepaired(true);
-
            car.setDateOfRepair(carDto.getDateOfRepair());
         } catch (Exception e) {
             return e.toString();
