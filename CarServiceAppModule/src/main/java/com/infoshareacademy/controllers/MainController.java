0fc0df55523eba7a3d1b9ae8284144bbefb7ea96
PRZWAN <wanagos@interia.eu>
2021-12-28 12:54:03 +0100

    handling file errors 2

--- CarServiceAppModule/src/main/java/com/infoshareacademy/controllers/MainController.java
+++ CarServiceAppModule/src/main/java/com/infoshareacademy/controllers/MainController.java
@@ -25,50 +25,9 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String mainPage() {
+    public String mainPage(Model model) {
         cars.setCars(Services.readCarService().getCars());
         if(exception!=null){
-          Model model=new Model() {
-              @Override
-              public Model addAttribute(String attributeName, Object attributeValue) {
-                  return null;
-              }
-
-              @Override
-              public Model addAttribute(Object attributeValue) {
-                  return null;
-              }
-
-              @Override
-              public Model addAllAttributes(Collection<?> attributeValues) {
-                  return null;
-              }
-
-              @Override
-              public Model addAllAttributes(Map<String, ?> attributes) {
-                  return null;
-              }
-
-              @Override
-              public Model mergeAttributes(Map<String, ?> attributes) {
-                  return null;
-              }
-
-              @Override
-              public boolean containsAttribute(String attributeName) {
-                  return false;
-              }
-
-              @Override
-              public Object getAttribute(String attributeName) {
-                  return null;
-              }
-
-              @Override
-              public Map<String, Object> asMap() {
-                  return null;
-              }
-          }  ;
           model.addAttribute("exception",exception.toString());
           return "file-error";
         }
